86e95a4b467d6b9a01318284735030a15e8c0cb1
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-03-02 15:28:50 +0100

    Melding AVG powershell
    had een gekke melding van avgdat die de powershell blokkeerd ofzo heb nu alles verwijderd en opnieuw begonnen.
    
    Het was iets met de vite.config.js file met de server ofzo

--- snowglobe-denmark/vite.config.js
+++ snowglobe-denmark/vite.config.js
@@ -3,11 +3,6 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-    },
     build:
     {
         outDir: '../dist', // Output in the dist/ folder
@@ -17,5 +12,5 @@ export default {
     plugins:
     [
         restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ]
+    ],
 }
