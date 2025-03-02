9f7d62fa715b89c343b082e8a9f58bd48fa81fde
Anna-Kyra <144000242+Anna-Kyra@users.noreply.github.com>
2025-03-02 13:32:16 +0100

    three.js setup

--- snowclobe-denmark/src/script.js
+++ snowclobe-denmark/src/script.js
@@ -1,1 +1,71 @@
-console.log('js works')
+import * as THREE from 'three'
+
+/**
+ * Base
+ */
+// Debug
+
+// Canvas 
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+// test cube
+const geometry = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red'})
+)
+scene.add(geometry)
+
+/**
+ * Lights
+ */
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
